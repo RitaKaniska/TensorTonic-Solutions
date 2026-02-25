30a1719ac5dd6f11d7de4b9d4fd7baef42741ab1
Duy Hưng <163378800+RitaKaniska@users.noreply.github.com>
2026-02-25 13:18:48 +0700

    Add solution for relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def relu(x):
+    """
+    Implement ReLU activation function.
+    """
+    # Write code here
+    x = np.asarray(x, dtype = float)
+    x = np.where(x > 0, x, 0)
+    return x
